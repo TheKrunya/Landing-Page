bf652e5790b17f80facf00f3f89677da0588b58a
TheKrunya <nkrupnykcode@gmail.com>
2019-10-20 00:33:11 +0300

    copyrights

--- client/components/landing.js
+++ client/components/landing.js
@@ -702,6 +702,10 @@ const Home = () => {
           <div className="row pt-5 mt-5 text-center">
             <div className="col-md-12">
               <div className="border-top pt-5" />
+              <p>
+        Copyright &copy;<script>document.write(new Date().getFullYear());</script>
+        All rights reserved | This template is made with <i className="icon-heart" aria-hidden="true" /> by <a href="https://colorlib.com" target="_blank" rel="noopener noreferrer">Colorlib</a>
+              </p>
             </div>
 
           </div>
